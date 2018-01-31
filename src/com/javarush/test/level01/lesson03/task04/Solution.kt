3492e46114b47b4bc11a7abf1b986a25984f4ae2
Hex <wolfgunmiller@gmail.com>
2018-01-31 10:46:14 +0300

    Java to Kotlin
    Begins

---
+++ src/com/javarush/test/level01/lesson03/task04/Solution.kt
@@ -0,0 +1,10 @@
+package com.javarush.test.level01.lesson03.task04
+
+fun main(args: Array<String>){
+    val name = "Roman"
+    val age = 26
+    val height = 172
+    println(name)
+    println(age)
+    println(height)
+}
