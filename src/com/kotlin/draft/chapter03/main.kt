87bbf3755818d257f52574917daac8756d0b3414
Hex <wolfgunmiller@gmail.com>
2018-02-15 04:51:01 +0300

    Kotlin in Action chapter 3

--- src/com/kotlin/draft/chapter03/main.kt
+++ src/com/kotlin/draft/chapter03/main.kt
@@ -9,13 +9,65 @@ fun main(args: Array<String>) {
     println(list.joinToString(" "))
     println(list.joinToString(postfix = ";", prefix = "# "))
 
-    println("Kotlin".last())
-    println(listOf("One", "Two", "Three").join(" "))
+    println("Kotlin".last())//>>>n//(Kt lib)
+    println(listOf("One", "Two", "Three").join(" "))//>>>One Two Three
 
     reportOperationCount()
 
-    println("Kotlin".last)
+    println("Kotlin".last)//>>>n
     val sb = StringBuilder("Kotlin?")
     sb.lastCharSb = '!'
-    println(sb)
+    println(sb)//>>>Kotlin!
+
+    val strings: List<String> = listOf("first", "second", "fourteenth")
+    println(strings.last())//>>>fourteenth
+    val numbers: Collection<Int> = setOf(1, 2, 14)
+    println(numbers.max())//>>>14
+
+    fun myArrayOf(vararg values: String) = arrayOf(*values) //модификатор vararg
+    val myArgs = myArrayOf("abra", "kadabra")//>>>[strings: , abra, kadabra]
+    val list2 = listOf("strings: ", *myArgs) // * - spread оператор
+    println(list2)
+
+    for((index, element) in numbers.withIndex()){
+        println("numbers[$index]: $element")
+    }
+    val (number, string) = 7 to "seven" //Pair инициализирует сразу две переменные
+
+    val map = mapOf(1.to("one"), //обычный вызов
+            number to string,
+            53 to "fifty-three") // инфиксный
+    infix fun Int.multiply(number: Int) = this * number
+    println(5 multiply 2)//>>>10
+
+
+    println("12.345-6.A".split("\\.|-".toRegex()))
+    println("12.345-6.A".split(".", "-"))
+    println("12.345-6.A".split('.', '-'))
+
+    fun parsePath(path: String){
+        val directory = path.substringBeforeLast("/")
+        val fullName = path.substringAfterLast("/")
+
+        val fileName = fullName.substringBeforeLast(".")
+        val extension = fullName.substringAfterLast(".")
+
+        println("Dir: $directory, name: $fileName, ext: $extension")
+    }
+    parsePath("com/kotlin/draft/chapter03/main.kt")
+    fun parsePathReg(path: String){
+        val regex = """(.+)/(.+)\.(.+)""".toRegex()
+        val matchResult = regex.matchEntire(path)
+        if (matchResult != null){
+            val (directory, filename, extension) = matchResult.destructured
+            println("Dir: $directory, name: $filename, ext: $extension")
+        }
+    }
+    parsePath("com/kotlin/draft/chapter03/main.kt")
+
+    val kotlinLogo = """| //
+                       .|//
+                       .|/ \"""
+    println(kotlinLogo.trimMargin("."))
+    println("""${'$'}99.9""")
 }
---
+++ src/com/kotlin/draft/chapter03/usersThing.kt
@@ -0,0 +1,20 @@
+package com.kotlin.draft.chapter03
+
+class User(val id: Int, val name: String, val address: String)
+
+fun User.validateBeforeSave(){
+    fun validate(value: String, fieldName: String){
+        if(value.isEmpty()){
+            throw IllegalArgumentException(
+                    "Can't save user $id: empty $fieldName")//к свойствам класса User можно обращаться напрямую
+        }
+    }
+    validate(name, "Name")
+    validate(address, "Address")
+}
+
+fun saveUser(user: User){
+    user.validateBeforeSave() //вызов функции-расширения
+
+    /*Сохранение пользователя в базу*/
+}
